be50a4524dd5a8aa23f2df9e842afd3c98e1db88
Calvin Alfrido <calvinalfrido@Calvins-MacBook-Pro.local>
2023-07-04 15:45:39 +0700

    feat: added user

--- src/user.ts
+++ src/user.ts
@@ -1,11 +1,21 @@
-import { getCookie } from './util/cookie';
+import { getCookie, setCookie } from './util/cookie';
 
 export class User {
     static getCurrentUser(): User | undefined {
         const user = getCookie('user');
 
-        if (!user) return user;
+        if (user === undefined) return user;
 
         return JSON.parse(user) as User;
+    }
+
+    constructor(private readonly _name: string) {
+        setCookie('user', JSON.stringify(this), {
+            'max-age': 3600,
+        });
+    }
+
+    get name() {
+        return this._name;
     }
 }
