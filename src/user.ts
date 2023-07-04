65b4f3517eb50425c4f42d9b9619cc47cb188d8d
Calvin Alfrido <calvinalfrido@Calvins-MacBook-Pro.local>
2023-07-04 16:14:20 +0700

    refactor: abstracted user

--- src/user.ts
+++ src/user.ts
@@ -1,6 +1,12 @@
 import { getCookie, setCookie } from './util/cookie';
 
-export class User {
+export abstract class User {
+    constructor(private readonly _name: string) {
+        setCookie('user', JSON.stringify(this), {
+            'max-age': 3600,
+        });
+    }
+
     static getCurrentUser(): User | undefined {
         const user = getCookie('user');
 
@@ -9,13 +15,17 @@ export class User {
         return JSON.parse(user) as User;
     }
 
-    constructor(private readonly _name: string) {
-        setCookie('user', JSON.stringify(this), {
-            'max-age': 3600,
-        });
-    }
-
     get name() {
         return this._name;
+    }
+}
+
+export function newUser(name: string): User {
+    return new DefaultUser(name);
+}
+
+export class DefaultUser extends User {
+    constructor(_name: string) {
+        super(_name);
     }
 }
